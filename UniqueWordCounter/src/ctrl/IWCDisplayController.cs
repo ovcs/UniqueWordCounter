ed7a741dccb94c129735f21f4176df7cc16f2887
OvcS <gerkules007@yahoo.com>
2022-09-06 13:52:00 +0300

    Made structure of project

---
+++ UniqueWordCounter/src/ctrl/IWCDisplayController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniqueWordCounter.src.ctrl
+{
+    internal interface IWCDisplayController
+    {
+        public void AllContentResult();
+        public void Errors();
+        public void Info();
+        public void LoadedFile();
+    }
+}
---
+++ UniqueWordCounter/src/mod/rep/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniqueWordCounter.src.mod.rep
+{
+    internal interface IRepository<T>
+    {
+        public void Add(T obj);
+        public int GetIDBy(T obj);
+
+        public List<T> GetAll();
+        public T GetBy(int ID);
+        public T Search(string value);
+
+        public InnerData Serialize();
+        public void Remove(T obj);
+    }
+}
