a3facea9b200a1c775af35a0bb8ba13dab7a7cb2
OvcS <gerkules007@yahoo.com>
2022-09-17 22:48:45 +0300

    Add FileSaver and Reader Component

---
+++ UniqueWordCounter/IO/Reader.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using UniqueWordCounter.Models.Data;
+
+namespace UniqueWordCounter.IO
+{
+    internal class Reader : ILoader
+    {
+        InnerData innerdata;
+        Encoding encoding;
+
+        public Reader(InnerData innerdata, Encoding encoding)
+        {
+            this.innerdata = innerdata;
+            this.encoding = encoding;
+        }
+
+        public Reader(InnerData innerdata) : this(innerdata, Encoding.UTF8)
+        {
+        }
+
+        public void Load(string path, IDeserializator format)
+        {
+            if (string.IsNullOrEmpty(path) || format == null)
+            {
+                throw new ArgumentNullException(nameof(format));
+            }
+
+            using StreamReader sr = new(path, encoding);
+            innerdata.Take(format.Deserialize(sr));
+        }
+        /*
+                ISerializator ser = GiveSerializator(
+            Regex.Match(path, @"(?i:^.*\.([\w]+)+$)").Value);
+        */
+    }
+}
