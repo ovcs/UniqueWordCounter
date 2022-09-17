76eeba2cbca487c0def393baccf4f09bed1bd082
OvcS <gerkules007@yahoo.com>
2022-09-17 22:47:47 +0300

    add Format component (for txt and fb2)

---
+++ UniqueWordCounter/IO/Format/FormatRepo.cs
@@ -0,0 +1,50 @@
+﻿namespace UniqueWordCounter.IO.Format
+{
+    internal class FormatRepo : ISerializeRepo
+    {
+        Dictionary<string, IDeserializator> deserializators;
+        Dictionary<string, ISerializator> serializators;
+        char formatChar = '.';
+
+        public FormatRepo(Dictionary<string, IDeserializator> deserializators,
+                          Dictionary<string, ISerializator> serializators)
+        {
+            this.deserializators = deserializators;
+            this.serializators = serializators;
+        }
+
+        public IDeserializator GetDeserializator(string format)
+        {
+            if (!format.StartsWith(formatChar))
+            {
+                throw new IOException("Incorrect file format");
+            }
+
+            if (deserializators.ContainsKey(format))
+            {
+                return deserializators[format];
+            }
+            else
+            {
+                throw new IOException("Invalid file format");
+            }
+        }
+
+        public ISerializator GetSerializator(string format)
+        {
+            if (!format.StartsWith(formatChar))
+            {
+                throw new IOException("Incorrect file format");
+            }
+
+            if (serializators.ContainsKey(format))
+            {
+                return serializators[format];
+            }
+            else
+            {
+                throw new IOException("Invalid file format");
+            }
+        }
+    }
+}
---
+++ UniqueWordCounter/IO/Format/Txt.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+
+namespace UniqueWordCounter.IO.Format
+{
+    internal class Txt : IDeserializator, ISerializator
+    {
+        public string SerializatorFormat => ".txt";
+
+        public object Deserialize(StreamReader stream)
+        {
+            return stream != null ? stream.ReadToEnd() : string.Empty;
+        }
+
+        public string Serialize(object tokens)
+        {
+            if (tokens.GetType() == typeof(List<string>))
+            {
+                List<string> list = (List<string>)tokens;
+                return new StringBuilder().AppendJoin("\n", list).ToString();
+            }
+            else
+            {
+                return string.Empty;
+            }
+        }
+
+        public override string ToString() => ".txt";
+    }
+}
