a2e9b5a000b89d2d97f3477e85933dcc4c235c38
OvcS <gerkules007@yahoo.com>
2022-09-17 22:38:28 +0300

    Add InnerData, WRepManager (Model Component)

---
+++ UniqueWordCounter/Models/Data/InnerData.cs
@@ -0,0 +1,30 @@
+﻿namespace UniqueWordCounter.Models.Data
+{
+    internal class InnerData : IHandlerData
+    {
+        List<string> strBuffer;
+
+        public Queue<string> queueData { get => new(strBuffer); }
+
+        public InnerData()
+        {
+        }
+
+        public void Take(object? obj)
+        {
+            if (obj == null)
+            {
+                return;
+            }
+
+            if (obj.GetType() == typeof(List<string>))
+            {
+                strBuffer = (List<string>) obj;
+            }
+            else
+            {
+                throw new InvalidCastException(obj.ToString());
+            }
+        }
+    }
+}
