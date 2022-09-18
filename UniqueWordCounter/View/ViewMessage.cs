9e535dd21f25cc4cf4850d702167ffd257fe5d9b
OvcS <gerkules007@yahoo.com>
2022-09-18 15:45:04 +0300

    fix message text

--- UniqueWordCounter/View/ViewMessage.cs
+++ UniqueWordCounter/View/ViewMessage.cs
@@ -2,12 +2,12 @@
 {
     public class ViewMessage
     {
-        public const string INTRO =             "Welcome to WordCounter \nMenu: \n1 - Start\n2 - Quit";
+        public const string INTRO =             "Welcome to WordCounter \nMenu: \n1 - Start\n0 - Quit";
         public const string LOAD_FILE =         "Please enter the path to load file:";
         public const string START_WC =          "Count started";
-        public const string TO_DO =             "Choose actions\n 1 - Show digit of max count word\n 2 - Save File";
+        public const string TO_DO =             "Choose actions\n 1 - Save File\n 0 - Quit";
         public const string SAVE_FILE =         "Please enter the path to save file:";
-        public const string REPEAT =            "Do you want try again?\n 1 - Yes\n 2 - No";
+        public const string REPEAT =            "Do you want try again?\n 1 - Yes\n 0 - Quit";
         public const string INVALID_STRING =    "Invalid path to the file";
         public const string BYE =               "Bye!";
     }
